f6799d56c971e0e507a5d06be1875bccb6b09833
Chris <cristru8@gmail.com>
2025-05-18 12:20:52 -0500

    first commit

---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,46 @@
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>(this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            // add generic dbcontext
+            services.AddDbContext<TContext>(option => option.UseSqlServer(config.GetConnectionString("eCommerceConnection"), sqlServerOption => sqlServerOption.EnableRetryOnFailure()));
+
+            // configure serilog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: $"{fileName}-.text", 
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information, 
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day
+                ).CreateLogger();
+
+            // add JWT auth scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            // Use global Exception
+            app.UseMiddleware<GlobalException>();
+
+            // register middleware to block all outsiders api calls
+            app.UseMiddleware<ListenToOnlyApiGateway>();
+
+            return app;
+        }
+    }
+}
---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Extract especific header from the request
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            // null means the request is not comming from the api gateway
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service is unavailable");
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
