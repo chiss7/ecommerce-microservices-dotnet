2c8e01773d927e4a521ed79c5ded02ac54f673b6
Chris <cristru8@gmail.com>
2025-05-24 11:42:12 -0500

    feat: add entities and DTOs in Domain & Application layers

---
+++ DemoEcommerce.OrderApiSolution/OrderApi.Application/DTOs/AppUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.DTOs
+{
+    public record AppUserDTO(
+        int Id,
+        [Required] string Name,
+        [Required] string Phone,
+        [Required] string Address,
+        [Required, EmailAddress] string Email,
+        [Required] string Password,
+        [Required] string Role
+    );
+}
